ab6bee130e1443d8a80b8f6d1247da7f3e4f135a
Admin <Admin@192.168.0.102>
2020-06-12 14:26:23 +0530

    added sysout msg

--- sample/src/sample/code/Sample.java
+++ sample/src/sample/code/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Hi WElcome to Devops");
+		System.out.println("Hi WElcome to Devops and Docker and Kubernetes");
 	}
 
 }
